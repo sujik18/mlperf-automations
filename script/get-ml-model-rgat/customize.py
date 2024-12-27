f3a54591d3eca1ad1aa39b7f98b97a3dc4c3e09d
Arjun Suresh <arjun@gateoverflow.com>
2024-12-27 12:06:26 +0000

    Fixes for podman support (#79)
    * Fix coco2014 sample ids path
    
    * Fixes for podman, #33
    
    * [Automated Commit] Format Codebase
    
    * Fixed rgat checkpoint path
    
    * Fixed rgat checkpoint path
    
    * [Automated Commit] Format Codebase

--- script/get-ml-model-rgat/customize.py
+++ script/get-ml-model-rgat/customize.py
@@ -7,10 +7,17 @@ def preprocess(i):
     os_info = i['os_info']
     env = i['env']
 
+    download_dir = env.get('CM_OUTDIRNAME', '')
+
     path = env.get('RGAT_CHECKPOINT_PATH', '').strip()
 
     if path == '' or not os.path.exists(path):
-        env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
+        if download_dir != '' and os.path.exists(
+                os.path.join(download_dir, "RGAT", "RGAT.pt")):
+            env['RGAT_CHECKPOINT_PATH'] = os.path.join(
+                download_dir, "RGAT", "RGAT.pt")
+        else:
+            env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
 
     return {'return': 0}
 
@@ -19,9 +26,12 @@ def postprocess(i):
 
     env = i['env']
 
+    if env.get('RGAT_CHECKPOINT_PATH', '') == '':
+        env['RGAT_CHECKPOINT_PATH'] = os.path.join(
+            env['RGAT_DIR_PATH'], "RGAT.pt")
+
     if env.get('CM_ML_MODEL_RGAT_CHECKPOINT_PATH', '') == '':
-        env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH'] = env.get(
-            'RGAT_CHECKPOINT_PATH', os.path.join(env['RGAT_DIR_PATH'], "RGAT.pt"))
+        env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH'] = env['RGAT_CHECKPOINT_PATH']
 
     if env.get('CM_ML_MODEL_PATH', '') == '':
         env['CM_ML_MODEL_PATH'] = env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH']
