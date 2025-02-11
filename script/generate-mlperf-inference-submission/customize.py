6b8814a9db0ff704611432ff819060a6002ccf8d
Arjun Suresh <arjun@gateoverflow.com>
2025-02-11 18:29:37 +0000

    Update customize.py | code cleanup

--- script/generate-mlperf-inference-submission/customize.py
+++ script/generate-mlperf-inference-submission/customize.py
@@ -521,13 +521,14 @@ def generate_submission(env, state, inp, submission_division):
                                 return {
                                     "return": 1, "error": f"user.conf missing in both paths: {user_conf_path} and {os.path.join(result_scenario_path, 'user.conf')}"}
 
+                    #First check for measurements directory in scenario folder
                     measurements_json_path = os.path.join(
                         result_scenario_path, "measurements.json")
                     target_measurement_json_path = measurement_scenario_path
+                    
                     if not os.path.exists(measurements_json_path):
                         measurements_json_path = os.path.join(
                             result_mode_path, "measurements.json")
-                        target_measurement_json_path = submission_measurement_path
 
                     if os.path.exists(measurements_json_path):
                         with open(measurements_json_path, "r") as f:
