43fba7d7948b37010f0749b1a9ee4e9063a21850
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-02-14 18:12:57 +0530

    enable download for 7 and 70B (#221)

--- script/get-ml-model-llama2/run-rclone.sh
+++ script/get-ml-model-llama2/run-rclone.sh
@@ -1,3 +1,3 @@
 rclone config create mlc-llama2 drive config_is_local=false scope=drive.readonly root_folder_id=11tBZvvrh0FCm3XuR5E849K42TqftYdUF
 rclone config reconnect mlc-llama2:
-rclone copy mlc-llama2:Llama-2-7b-chat-hf ${LLAMA2_CHECKPOINT_PATH}/Llama-2-7b-chat-hf -P
+rclone sync mlc-llama2:${MLC_GIT_CHECKOUT_FOLDER} ${LLAMA2_CHECKPOINT_PATH}/${MLC_GIT_CHECKOUT_FOLDER} -P
