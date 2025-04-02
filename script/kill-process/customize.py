8be31b5faf75f1966d9b12f2a9336cf5925f62f3
Arjun Suresh <arjun@gateoverflow.com>
2025-04-02 12:47:11 +0100

    Added kill process script (#347)
    * Added kill process MLC script

---
+++ script/kill-process/customize.py
@@ -0,0 +1,52 @@
+from mlc import utils
+import os
+import subprocess
+
+
+def generate_kill_command(env):
+    kill_cmd = ""
+
+    if env.get("MLC_KILL_PROCESS_GROUP"):
+        if env.get("MLC_KILL_PROCESS_ID"):
+            process_id = env["MLC_KILL_PROCESS_ID"]
+            # Find process group and kill all processes in it
+            kill_cmd = f"pkill -g $(ps -o pgid= -p {process_id} | tr -d ' ')"
+
+        elif env.get("MLC_KILL_PROCESS_NAME"):
+            process_name = env["MLC_KILL_PROCESS_NAME"]
+            # Find process group of the first matching process name and kill
+            # all in that group
+            kill_cmd = f"pkill -g $(pgrep -o {process_name} | xargs ps -o pgid= -p | tr -d ' ')"
+
+    else:
+        if env.get("MLC_KILL_PROCESS_ID"):
+            process_id = env["MLC_KILL_PROCESS_ID"]
+            kill_cmd = f"kill {process_id}"  # Kill a single process by ID
+
+        elif env.get("MLC_KILL_PROCESS_NAME"):
+            process_name = env["MLC_KILL_PROCESS_NAME"]
+            # Kill all processes matching the name
+            kill_cmd = f"pkill {process_name}"
+
+    env["MLC_RUN_CMD"] = kill_cmd if kill_cmd else "echo 'No valid input provided'"
+
+
+def preprocess(i):
+
+    env = i['env']
+    state = i['state']
+    os_info = i['os_info']
+
+    generate_kill_command(env)
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+    state = i['state']
+
+    os_info = i['os_info']
+
+    return {'return': 0}
