ade0cb0f7e6bd3f2ad365b5003e9dd50dad31fbe
Arjun <arjunsuresh1987@gmail.com>
2025-05-14 23:27:46 +0000

    Support ifort install in oneapi

--- script/get-oneapi/install.sh
+++ script/get-oneapi/install.sh
@@ -7,3 +7,10 @@ cmd="bash ./${MLC_ONEAPI_INSTALL_FILENAME} -a --silent --cli --eula accept  --in
 
 echo $cmd
 eval $cmd
+
+if [[ ${MLC_ONEAPI_FORTRAN} == 'yes' ]] then
+  wget -nc https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2238465b-cfc7-4bf8-ad04-e55cb6577cba/intel-fortran-essentials-2025.1.1.8_offline.sh
+  cmd="bash ./intel-fortran-essentials-2025.1.1.8_offline.sh -a --silent --cli --eula accept --install-dir ${PWD}/install"
+  echo $cmd
+  eval $cmd
+fi
