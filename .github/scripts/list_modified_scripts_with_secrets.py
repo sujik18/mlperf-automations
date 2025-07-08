cfbf3d774f59c153018c5a790066b1521d0c727b
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-07-08 19:02:00 +0530

    Fix for non interactive rclone-GDrive downloads (#489)
    * Fix for non interactive rclone-GDrive downloads
    
    * add seperate  github action for testing sripts with modified meta

--- .github/scripts/list_modified_scripts_with_secrets.py
+++ .github/scripts/list_modified_scripts_with_secrets.py
@@ -9,7 +9,7 @@ def get_file_info(filepath):
         content = yaml.safe_load(file)
         tests = content.get('tests', {})
         needs_pat = tests.get('needs_pat', False)
-        if tests and not needs_pat:
+        if tests and needs_pat:
             num_tests = len(tests.get('run_inputs', []))
         else:
             num_tests = 0
@@ -31,24 +31,8 @@ def process_files(files):
     ]
 
 
-def get_modified_metas(files):
-    filenames = files.split(",")
-    return [
-        {
-            "file": file,
-            "uid": uid,
-        }
-        for file in filenames if os.path.basename(file) == 'meta.yaml'
-        for uid, num_tests in [get_file_info(file)]
-    ]
-
-
 if __name__ == "__main__":
     changed_files = sys.stdin.read().strip()
     processed_files = process_files(changed_files)
-    modified_metas = get_modified_metas(changed_files)
     json_processed_files = json.dumps(processed_files)
     print(json_processed_files)
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
-        f.write(
-            f"processed_files={json.dumps({'file_info': processed_files, 'modified_metas': modified_metas})}\n")
