619734d36aca93d69908e9bb7b520b6340572d2e
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-09-09 19:15:15 +0530

    Update run.sh

--- script/get-preprocessed-dataset-openorca/run.sh
+++ script/get-preprocessed-dataset-openorca/run.sh
@@ -5,9 +5,11 @@ echo "${MLC_RUN_CMD}"
 eval "${MLC_RUN_CMD}"
 test $? -eq 0 || exit $?
 
-if [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "true" ] || \
-   [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "yes" ] || \
-   [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "1" ]; then
+if { [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "true" ] || \
+     [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "yes" ] || \
+     [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "1" ]; } && \
+   [ "${MLC_DATASET_CALIBRATION}" = "yes" ]; then
+
     ${MLC_PYTHON_BIN_WITH_PATH} ${MLC_TMP_CURRENT_SCRIPT_PATH}/nvidia_preprocess.py \
         -d ${MLC_OPENORCA_PREPROCESSED_ROOT} \
         -o ${MLC_OPENORCA_PREPROCESSED_ROOT}/preprocessed_data
