f43bebb1aa7052103acb965589b3a03e2abf165a
anandhu-eng <anandhusooraj011@gmail.com>
2025-09-10 13:31:54 +0530

    fix typo

--- script/get-preprocessed-dataset-openorca/run.sh
+++ script/get-preprocessed-dataset-openorca/run.sh
@@ -5,9 +5,9 @@ echo "${MLC_RUN_CMD}"
 eval "${MLC_RUN_CMD}"
 test $? -eq 0 || exit $?
 
-if   [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "true" ] || \
-     [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "yes" ] || \
-     [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "1" ]; ]; then
+if [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "true" ] || \
+   [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "yes" ] || \
+   [ "${MLC_DATASET_PREPROCESSED_BY_MLC}" = "1" ]; then
     ${MLC_PYTHON_BIN_WITH_PATH} ${MLC_TMP_CURRENT_SCRIPT_PATH}/nvidia_preprocess.py \
         -d ${MLC_OPENORCA_PREPROCESSED_ROOT} \
         -o ${MLC_OPENORCA_PREPROCESSED_ROOT}/preprocessed_data
