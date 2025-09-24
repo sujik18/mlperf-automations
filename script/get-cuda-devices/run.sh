ab6249733b0768eb8859778b7e1ac9f11b60a668
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-09-25 01:30:31 +0530

    Fix issue with temp file paths (#648)
    * Fix issue with temp file paths
    
    * fix typoe

--- script/get-cuda-devices/run.sh
+++ script/get-cuda-devices/run.sh
@@ -18,9 +18,7 @@ echo ""
 echo "Compiling program ..."
 echo ""
 
-cd ${MLC_TMP_CURRENT_SCRIPT_PATH}
-
-${MLC_NVCC_BIN_WITH_PATH} -allow-unsupported-compiler print_cuda_devices.cu
+${MLC_NVCC_BIN_WITH_PATH} -allow-unsupported-compiler ${MLC_TMP_CURRENT_SCRIPT_PATH}/print_cuda_devices.cu
 test $? -eq 0 || exit 1
 
 # Return to the original path obtained in MLC
@@ -29,7 +27,5 @@ echo ""
 echo "Running program ..."
 echo ""
 
-cd ${MLC_TMP_CURRENT_PATH}
-
-${MLC_TMP_CURRENT_SCRIPT_PATH}/a.out > tmp-run.out
+./a.out > tmp-run.out
 test $? -eq 0 || exit 1
