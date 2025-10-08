2b33ec891cdab0a629d596d60f7e8655e408980f
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-10-08 12:16:28 +0530

    Support r2-downloader for nuscenes dataset download (#667)
    * Initial changes for supporting r2-downloader for nuscenes dataset
    
    * code formatting and update for r2-downloader

--- script/get-preprocessed-dataset-nuscenes/customize.py
+++ script/get-preprocessed-dataset-nuscenes/customize.py
@@ -19,18 +19,26 @@ def postprocess(i):
     env = i['env']
 
     if is_true(env.get('MLC_TMP_REQUIRE_DOWNLOAD', '')):
-        env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'] = os.path.join(
-            env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'],
-            env['MLC_DATASET_NUSCENES_EXTRACTED_FOLDER_NAME'])
-        if env.get(
-                'MLC_PREPROCESSED_DATASET_NUSCENES_SCENE_LENGTHS_PATH', '') != '':
-            shutil.copy(
-                os.path.join(
-                    env['MLC_PREPROCESSED_DATASET_NUSCENES_SCENE_LENGTHS_PATH'],
-                    env['MLC_DATASET_NUSCENES_SCENE_PICKLE_FILENAME']),
-                os.path.join(
-                    os.path.dirname(
-                        env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'].rstrip("/")),
-                    env['MLC_DATASET_NUSCENES_SCENE_PICKLE_FILENAME']))
+        if env.get('MLC_DOWNLOAD_TOOL', '') == "rclone":
+            env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'] = os.path.join(
+                env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'],
+                env['MLC_DATASET_NUSCENES_EXTRACTED_FOLDER_NAME'])
+        elif env.get('MLC_DOWNLOAD_TOOL', '') == "r2-downloader":
+            env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'] = os.path.join(
+                env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'],
+                "preprocessed",
+                env['MLC_DATASET_NUSCENES_EXTRACTED_FOLDER_NAME'])
+
+        if env.get('MLC_DOWNLOAD_TOOL', '') == "rclone":
+            if env.get(
+                    'MLC_PREPROCESSED_DATASET_NUSCENES_SCENE_LENGTHS_PATH', '') != '':
+                shutil.copy(
+                    os.path.join(
+                        env['MLC_PREPROCESSED_DATASET_NUSCENES_SCENE_LENGTHS_PATH'],
+                        env['MLC_DATASET_NUSCENES_SCENE_PICKLE_FILENAME']),
+                    os.path.join(
+                        os.path.dirname(
+                            env['MLC_PREPROCESSED_DATASET_NUSCENES_PATH'].rstrip("/")),
+                        env['MLC_DATASET_NUSCENES_SCENE_PICKLE_FILENAME']))
 
     return {'return': 0}
