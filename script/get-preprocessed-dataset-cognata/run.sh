b9f7bb2ca076000b0c2bc38df899328463ee67d1
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-10-10 00:36:04 +0530

    Use r2-downloader for cognata dataset (#668)

--- script/get-preprocessed-dataset-cognata/run.sh
+++ script/get-preprocessed-dataset-cognata/run.sh
@@ -3,7 +3,7 @@
 if [[ "$MLC_DOWNLOAD_MODE" != "dry" && "$MLC_TMP_REQUIRE_DOWNLOAD" = "yes" ]]; then
   cd "${MLC_PREPROCESSED_DATASET_COGNATA_PATH}" || exit
   for f in *.tar.gz; do 
-    tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
+    tar --no-same-owner -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
   done
   cd - || exit
 fi
